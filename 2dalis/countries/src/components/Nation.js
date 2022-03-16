96f33481abc800ca66de14866ba357bef42458b1
Ignblue <95609967+Ignblue@users.noreply.github.com>
2022-03-16 22:58:43 +0200

    Add files via upload

--- 2dalis/countries/src/components/Nation.js
+++ 2dalis/countries/src/components/Nation.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-const Nation= ({nation}) =>{
+const Nation= ({ nation }) =>{
     return (
         <div>
-        <h1>{nation.name}</h1>
+            <h1>{nation.name.official}</h1>
+            <div>capital {nation.capital}</div>
+            <div>region {nation.region}</div>
+            <h3>languages</h3>
+            
+   <ul>
+      {Object.entries(nation.languages).map(
+        language => <li key={language.name}>{language.name}</li>
+      )}
+      </ul>
         </div>
     )
 }
--- 2dalis/countries/src/components/Nations.js
+++ 2dalis/countries/src/components/Nations.js
@@ -3,11 +3,20 @@ import Nation from "./Nation";
 
 
 const Nations = ({nationstoshow,}) => {
-  if(nationstoshow.length<=10){
-    return ( <Nation nation={nationstoshow} /> )
+  if (nationstoshow.length <= 10)
+  {
+    return (
+      <div>
+        {nationstoshow.map((ele, i) => <Nation key={i} nation={ele} />)}
+      </div>
+    );
   }
-  else {
-    return ( <div>Too many matches, specify another filter</div>  )
+   else{
+   return (
+      <div>Too many matches, specify another filter</div>
+    );
   }
 }
-export default Nations;
+
+
+export default Nations; 
