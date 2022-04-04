498a24b1658f81bcd6341dbd5549fab895dee06b
Ignblue <95609967+Ignblue@users.noreply.github.com>
2022-04-04 12:03:23 +0300

    Delete 2dalis directory

--- 2dalis/phonebook/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-const Filter = ({ title, name, handleFunction }) => {
-  return (
-    <div> {title} <input value={name} onChange={handleFunction}/>
-    </div>
-  )
-}
-
-export default Filter
