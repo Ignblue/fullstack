3e705b74d2d87c641c1a6ab16002d53396ed6cce
Ignblue <95609967+Ignblue@users.noreply.github.com>
2022-04-04 11:40:32 +0300

    Add files via upload
    3.9

--- 3dalis/index.js
+++ 3dalis/index.js
@@ -1,92 +1,85 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
+
+morgan.token('person', (request, response) => JSON.stringify(request.body));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
 let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
+  { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
 ]
 
-app.get('/info', (request, response) =>
-{
-  response.send(`<div>Phonebook has info for ${persons.length} people </div>
+app.get('/info', (request, response) => {
+response.send(`<div>Phonebook has info for ${persons.length} people </div>
 <div> ${new Date()} </div>`)
 })
 
-app.get('/api/persons', (request, response) =>
-{
+app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
-app.get('/api/persons/:id', (request, response) =>
-{
+app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person)
-  {
-    response.json(person)
-  }
-  else
-  {
-    response.status(404).end()
-  }
+  const person = persons.find(person =>  person.id === id)
+ if (person) {
+   response.json(person)
+ } else {
+   response.status(404).end()
+ }
 })
 
-app.delete('/api/persons/:id', (request, response) =>
-{
+app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
 })
 
-const generateId = () =>
-{
+const generateId = () => {
   const maxId = persons.length > 0
-    ? Math.floor(Math.random(...persons.map(n => n.id)) * 1000)
+    ? Math.floor(Math.random(...persons.map(n => n.id))*1000)
     : 0
   return maxId
 }
 
-app.post('/api/persons', (request, response) =>
-{
+app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  //The name is missing
-  if (!body.name)
-  {
-    return response.status(400).json({
-      error: 'name missing'
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
     })
   }
-
-  //The number is missing
-  if (!body.number)
-  {
-    return response.status(400).json({
-      error: 'number missing'
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
     })
   }
-
+  
   //The name already exists in the phonebook
   const result1 = persons.find((ele) => ele.name === body.name)
 
@@ -96,20 +89,19 @@ app.post('/api/persons', (request, response) =>
       error: 'name already existing'
     })
   }
-
-  //Add new person
+ 
   const person = {
     id: generateId(),
     name: body.name,
     number: body.number
   }
 
-  persons.push(person)
+  persons = persons.concat(person)
 
-  //
   response.json(person)
 })
 
-const PORT = 3001
-app.listen(PORT, "127.0.0.1")
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
